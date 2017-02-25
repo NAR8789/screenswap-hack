affd1df1ae48e3b82a148feaa450acd0eefab522
Xiao Fan <xiao.h.fan@gmail.com>
2017-02-24 20:49:22 -0800

    separate path and aliases

---
+++ aliases.zsh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+alias xo=xoff
+alias xe='xoff eDP-1'
+alias xd='xoff DP-1'
--- path.zsh
+++ path.zsh
@@ -1,7 +1,3 @@
 #!/usr/bin/env zsh
 
 PATH="$(dirname "${(%):-%N}")/bin:$PATH"
-
-alias xo=xoff
-alias xe='xoff eDP-1'
-alias xd='xoff DP-1'
